5ac0629f77b4ac8d8f86de4d42d8e54f023d9ecc
JTokos <33537516+JTokos@users.noreply.github.com>
2020-02-17 21:47:52 -0800

    Added files

---
+++ lib/Constants.dart
@@ -0,0 +1,11 @@
+class Constants{
+  static const String Setting = 'Settings';
+  static const String About = 'About';
+  static const String Version = 'Version';
+
+  static const List<String> choices = <String>[
+    Setting,
+    About,
+    Version,
+  ];
+}
---
+++ lib/main.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+
+import 'Constants.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Bamboo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Bamboo'),
+          actions: <Widget>[
+            PopupMenuButton<String>(
+              onSelected: choiceAction,
+              itemBuilder: (BuildContext context){
+                return Constants.choices.map((String choice){
+                  return PopupMenuItem<String>( //settings menu
+                    value: choice,
+                    child: Text(choice),
+                  );
+                }).toList();
+              },//itemBuilder
+            )
+          ],
+        ),
+        body:
+        BodyLayout(),
+      ),
+    );
+  }
+}
+
+void choiceAction(String choice){
+  print('Working');
+}
+
+class BodyLayout extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return _myListView(context);
+  }
+}
+
+Widget _myListView(BuildContext context) {
+  return ListView(
+    children: ListTile.divideTiles(
+      context: context,
+      tiles: [
+        ListTile(
+          title: Text('Level 1'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 1 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 2'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 2 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 3'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 3 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 4'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 4 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 5'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 5 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 6'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 6 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 7'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 7 Selected');
+          },
+        ),
+        ListTile(
+          title: Text('Level 8'),
+          trailing: Icon(Icons.keyboard_arrow_right),
+          onTap: () {
+            print('Level 8 Selected');
+          },
+        ),
+      ],
+    ).toList(),
+  );
+}
